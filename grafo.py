31b31de06062f2262cfa5b9d607806c9aa00d6f2
Danteeh <boingochavez1@gmail.com>
2025-09-27 16:23:09 -0500

    Feat- Menu and all metods for graphs

--- grafo.py
+++ grafo.py
@@ -32,7 +32,6 @@ class Grafo:
             adyacencia[arista.origen].append((arista.destino, arista.attrs))
             if not self.dirigido:
                 adyacencia[arista.destino].append((arista.origen, arista.attrs))
-
         return adyacencia
 
     def lista_incidencia(self):
@@ -44,20 +43,116 @@ class Grafo:
             else:
                 incidencia[arista.destino].append(arista)  
         return incidencia
+    
+    def matriz_adyacencia(self):
+        nodos = list(self.nodos.keys())             
+        idx = {n: i for i, n in enumerate(nodos)}  
+        n = len(nodos)
+        M = [[0] * n for _ in range(n)]              
+
+        for a in self.aristas:
+            i = idx[a.origen]
+            j = idx[a.destino]
+            M[i][j] = 1
+            if not self.dirigido:
+                M[j][i] = 1
+        return M, nodos
+    
+    def matriz_incidencia(self):
+        nodos = list(self.nodos.keys())        
+        aristas = self.aristas                 
+        n = len(nodos)
+        m = len(aristas)
+        
+        idx_nodo = {n: i for i, n in enumerate(nodos)}  
+        M = [[0] * m for _ in range(n)]
+        
+        for j, arista in enumerate(aristas):
+            i_origen = idx_nodo[arista.origen]
+            i_destino = idx_nodo[arista.destino]
+            
+            if self.dirigido:
+                M[i_origen][j] = -1
+                M[i_destino][j] = 1
+            else:
+                M[i_origen][j] = 1
+                M[i_destino][j] = 1
+        return M, nodos, aristas
 
 
-k2 = Grafo(dirigido=True)
-k2.agregar_nodo("M1")
-k2.agregar_nodo("M2")
-k2.agregar_arista("M1", "M2", {"Peso": 5})
-k2.agregar_nodo("M3")
-k2.agregar_arista("M2","M1",{"Peso":5})
-print(k2)
-print(k2.aristas)
+def menu():
+    print("\n--- Menu ---")
+    print("1. Crear grafo dirigido")
+    print("2. Crear grafo no dirigido")
+    print("3. Agregar nodo")
+    print("4. Agregar arista")
+    print("5. Mostrar lista de adyacencia")
+    print("6. Mostrar lista de incidencia")
+    print("7. Mostrar matriz de adyacencia")
+    print("8. Mostrar matriz de incidencia")
+    print("9. Salir")
 
-print("\nLista de adyacencia:")
-print(k2.lista_adyacencia())
+grafo = None
 
-print("\nLista de incidencia:")
-print(k2.lista_incidencia())
+while True:
+    menu()
+    opcion = input("Seleccione una opcion: ")
 
+    if opcion == "1":
+        grafo = Grafo(dirigido=True)
+        print("Grafo dirigido creado.")
+    elif opcion == "2":
+        grafo = Grafo(dirigido=False)
+        print("Grafo no dirigido creado.")
+    elif opcion == "3":
+        if grafo:
+            nodo = input("Ingrese el id del nodo: ")
+            grafo.agregar_nodo(nodo)
+            print(f"Nodo {nodo} agregado.")
+        else:
+            print("Primero cree un grafo.")
+    elif opcion == "4":
+        if grafo:
+            origen = input("Ingrese nodo origen: ")
+            destino = input("Ingrese nodo destino: ")
+            peso = input("Ingrese peso (opcional, ENTER si no): ")
+            attrs = {"Peso": int(peso)} if peso else {}
+            grafo.agregar_arista(origen, destino, attrs)
+            print(f"Arista {origen} -> {destino} agregada.")
+        else:
+            print("Primero cree un grafo.")
+    elif opcion == "5":
+        if grafo:
+            print("\nLista de adyacencia:")
+            print(grafo.lista_adyacencia())
+        else:
+            print("Primero cree un grafo.")
+    elif opcion == "6":
+        if grafo:
+            print("\nLista de incidencia:")
+            print(grafo.lista_incidencia())
+        else:
+            print("Primero cree un grafo.")
+    elif opcion == "7":
+        if grafo:
+            M, nodos = grafo.matriz_adyacencia()
+            print("\nMatriz de adyacencia:")
+            print("   " + "  ".join(nodos))
+            for i, fila in enumerate(M):
+                print(f"{nodos[i]} {fila}")
+        else:
+            print("Primero cree un grafo.")
+    elif opcion == "8":
+        if grafo:
+            Mi, nodos, aristas = grafo.matriz_incidencia()
+            print("\nMatriz de incidencia:")
+            print("       " + "  ".join([f"e{j}" for j in range(len(aristas))]))
+            for i, fila in enumerate(Mi):
+                print(f"{nodos[i]}  {fila}")
+        else:
+            print("Primero cree un grafo.")
+    elif opcion == "9":
+        print("Saliendo...")
+        break
+    else:
+        print("Opcion invalida.")
