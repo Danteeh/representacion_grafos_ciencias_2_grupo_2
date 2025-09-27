c4c67866a85ad6a0bd53e524d0954d1543082fe0
SaraRodriguez11 <168302983+SaraRodriguez11@users.noreply.github.com>
2025-09-26 22:04:36 -0500

    matriz_adyacencia

--- grafo.py
+++ grafo.py
@@ -25,6 +25,24 @@ class Grafo:
     def __repr__(self):
         tipo = "Dirigido" if self.dirigido else "No dirigido"
         return f"<Grafo {tipo}: {len(self.nodos)} nodos, {len(self.aristas)} aristas>"
+
+
+    def matriz_adyacencia(self):
+       
+        nodos = list(self.nodos.keys())             
+        idx = {n: i for i, n in enumerate(nodos)}  
+        n = len(nodos)
+        M = [[0] * n for _ in range(n)]              
+
+        for a in self.aristas:
+            i = idx[a.origen]
+            j = idx[a.destino]
+            M[i][j] = 1
+            if not self.dirigido:
+                M[j][i] = 1
+
+        return M, nodos
+    
     
     def lista_incidencia(self):
         incidencia = {nodo: [] for nodo in self.nodos}  
@@ -47,3 +65,17 @@ print(k2.aristas)
 
 print(k2.lista_incidencia())
 
+
+#prueba adyacencia
+if __name__ == "__main__":
+    g = Grafo(dirigido=False)
+    g.agregar_nodo("A")
+    g.agregar_nodo("B")
+    g.agregar_nodo("C")
+    g.agregar_arista("A", "B")
+    g.agregar_arista("B", "C")
+
+    M, nodos = g.matriz_adyacencia()
+    print("Nodos orden:", nodos)
+    for fila in M:
+        print(fila)
